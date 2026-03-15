2c7e70dfd4bfd6acfbce5155b23f7a3037309e87
shiv7898 <shivdhakad202@gmail.com>
2026-03-15 18:35:05 +0530

    added reverce string

--- 1ReverseString.js
+++ 1ReverseString.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+console.log("hello world")
+function reverceString(s){
+    const str=s
+    console.log(str)
+    for(i=str.length-1;i>0;i--){
+        // console.log(str[i])
+         process.stdout.write(str[i] + " ");
+
+    }
+    
+}
+const str="thisismyfirstcode"
+reverceString(str)
